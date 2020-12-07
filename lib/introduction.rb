ef6115a726a090bceb9567ea979210e6493a9503
Cassandra <cparisi1290@gmail.com>
2020-12-07 17:06:48 -0500

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+def introduction_with_languages(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_languages(Dan, Ember.js)
